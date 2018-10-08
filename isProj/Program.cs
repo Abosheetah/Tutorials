4ba06df23a124e5659934323dd3a2afdf476733e
= <=>
2018-10-08 10:12:06 +0200

    Add is project

---
+++ isProj/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace isProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            B b = new  B();
+            bool x = b is A ;
+            Console.WriteLine(x);
+        }
+    }
+
+
+    class A{
+        public int ID { get; set; }
+        public string Name { get; set; }        
+    }
+
+
+    class B : A {
+        public float Speed { get; set; }
+    }
+}
