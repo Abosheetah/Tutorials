b53ffb1ac06699e4138f0f0e186be2b924018f44
= <=>
2018-10-08 18:14:03 +0200

    Add nullable project

---
+++ Nullable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? x = null ;
+            if (x.HasValue)
+            {
+                Console.WriteLine($"the value is : {x}");                
+            }
+            else
+            {
+                Console.WriteLine("not exist any value.");
+            }
+
+            int? r = 50;
+
+            int rr = r ?? 1;
+
+            Console.WriteLine(rr);
+
+        }
+    }
+}
