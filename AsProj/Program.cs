e57cad089ca65776628eb9091af8745b8c4895ca
= <=>
2018-10-08 11:38:14 +0200

    Add as project

---
+++ AsProj/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AsProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 200;
+            byte? y = x as byte?;
+            Console.WriteLine(y.HasValue);
+        }
+    }
+}
