96be5179d68a6e3afa2fca71b023a411b50aa8b2
= <=>
2018-10-09 09:55:56 +0200

    Add extension method example

---
+++ ExtensionMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExtensionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "mohamed" ;
+            Console.WriteLine(name.toUpper80());
+            Console.ReadLine();
+        }
+    }
+
+    
+    public static class MyExtensionMethods{
+        public static string toUpper80(this string text){
+            return text + " !";
+        }
+    }
+}
