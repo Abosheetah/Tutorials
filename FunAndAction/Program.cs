8a2f20d01ae90a3b6785660e528c1612b1276d65
= <=>
2018-10-10 10:07:31 +0200

    add Function example

---
+++ FunAndAction/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+namespace FunAndAction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Action<int>[] actions = new Action<int>[3];
+            actions[0] = new Action<int>((x) => {Console.WriteLine("Action {0}" , x);});
+            actions[1] = new Action<int>((x) => {Console.WriteLine("Action {0}" , x);});
+            actions[2] = new Action<int>((x) => {Console.WriteLine("Action {0}" , x);});
+
+            ConvertToParallel(actions);
+            Console.ReadLine();
+        }
+
+        static void ConvertToParallel(Action<int>[] actions){
+            foreach (var item in actions)
+            {
+                Task.Run(() => {item(1);});
+            }
+        }
+
+        static void PrintHello(){
+            Console.WriteLine("Hello ");
+        }
+        
+        static void PrintWellcom(){
+            Console.WriteLine("Wellcom ");
+        }
+        
+        static void PrintHi(){
+            Console.WriteLine("Hi ");
+        }
+    }
+}
